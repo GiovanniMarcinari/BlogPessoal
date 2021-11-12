8e1db017c33ca7f3e12d7c098f5112ec106aa62b
GiovanniMarcinari <gmarcinari@gmail.com>
2021-11-11 23:53:52 -0300

    Adicionando Security

---
+++ src/main/java/br/org/generation/Blogpessoal/model/Usuario.java
@@ -0,0 +1,5 @@
+package br.org.generation.Blogpessoal.model;
+
+public class Usuario {
+
+}
--- src/main/java/br/org/generation/Blogpessoal/repository/TemaRepository.java
+++ src/main/java/br/org/generation/Blogpessoal/repository/TemaRepository.java
@@ -1,5 +1,15 @@
 package br.org.generation.Blogpessoal.repository;
 
-public class TemaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.Blogpessoal.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+    
+    public List <Tema> findAllByDescricaoContainingIgnoreCase (String descricao);
 
 }
